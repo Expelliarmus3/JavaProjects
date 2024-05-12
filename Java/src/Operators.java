4237331f8265446cb5f049a9e49fa758417ccd71
Ahana Sarkar <115629352+Expelliarmus3@users.noreply.github.com>
2024-05-12 10:24:59 +0530

    Second commit

--- Java/src/Operators.java
+++ Java/src/Operators.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Operators {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int i1= input.nextInt();
 
+        if (i1%2==0)
+            System.out.println(i1+" is even");
+        if (i1%2 !=0)
+            System.out.println(i1+" is odd");
     }
 }

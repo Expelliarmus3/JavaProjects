58c36c78f5eb8fb90d2d8039338306f7539bf219
Ahana Sarkar <115629352+Expelliarmus3@users.noreply.github.com>
2024-05-12 10:39:01 +0530

    Third commit

--- Java/src/Operators.java
+++ Java/src/Operators.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+//code using only if
+
 public class Operators {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -10,5 +12,18 @@ public class Operators {
             System.out.println(i1+" is even");
         if (i1%2 !=0)
             System.out.println(i1+" is odd");
+
+
+//another way for the previous code using if-else
+
+        Scanner v = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int i2= v.nextInt();
+
+        if (i2%2==0)
+            System.out.println(i2+" is even");
+        else
+            System.out.println(i2+" is odd");
+
     }
 }

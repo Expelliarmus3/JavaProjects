3a12715f8581172f3042d195bba0ecf679a240e1
Ahana Sarkar <115629352+Expelliarmus3@users.noreply.github.com>
2024-05-17 21:34:49 +0530

    Second commit

--- JAVA PRACTICE/src/Main.java
+++ JAVA PRACTICE/src/Main.java
@@ -1,15 +1,69 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+//pattern 1
+        for (int i=1;i<=n;i++) {
+            for (int j = 1; j <= n-i; j++)
+                System.out.print("*");
+            for (int j=1;j<=2*i-1;j++)
+                System.out.print(" ");
+            System.out.println();
+        }
+//pattern 2
+        for (int i=1;i<=n;i++) {
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+                System.out.print("*");
+            System.out.println();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        }
+
+        System.out.println();
+//pattern 3
+        for (int i=1;i<=n;i++) {
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+            for (int j = 1; j <=  i ; j++)
+                System.out.print("*");
+            System.out.println();
+
+        }
+        System.out.println();
+
+//pattern 4
+        for (int i=n;i>=1;i--) {
+            for (int j =1;j<=n-i ; j++)
+                System.out.print(" ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+                System.out.print("*");
+            System.out.println();
+
+        }
+        System.out.println();
+
+//pattern 5
+        for (int i=1;i<=n;i++) {
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+                System.out.print("*");
+            System.out.println();
+
+        }
+        for (int i=n;i>=1;i--) {
+            for (int j =1;j<=n-i ; j++)
+                System.out.print(" ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+                System.out.print("*");
+            System.out.println();
+
         }
     }
+
 }

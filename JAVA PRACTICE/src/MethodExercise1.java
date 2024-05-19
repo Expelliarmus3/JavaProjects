cf166c29f1cf1534be817efb7f55e38f49539431
Ahana Sarkar <115629352+Expelliarmus3@users.noreply.github.com>
2024-05-19 12:02:56 +0530

    Fourth commit

---
+++ JAVA PRACTICE/src/MethodExercise1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MethodExercise1 {
+    public static void main(String[] args) {
+        System.out.print("Enter your name and age: ");
+        System.out.println(getUserName()+" "+getUserAge());
+    }
+    public static String getUserName() {
+        Scanner s=new Scanner(System.in);
+        return s.nextLine();
+    }
+    public static int getUserAge() {
+        Scanner s=new Scanner(System.in);
+        return  s.nextInt();
+
+    }
+}
